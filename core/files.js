5a19a55ac5d5bc9443386da4551c30fed72a8cd3
nobody <5514211-nobody42@users.noreply.gitlab.com>
2020-03-17 07:39:17 +0100

    Fixed: Moment.js

--- core/files.js
+++ core/files.js
@@ -71,6 +71,7 @@ var files = {
         'resources/modernizr/2.8.3/modernizr.min.jsm': true,
 
         // Moment.js
+        'resources/moment.js/2.24.0/moment.min.js': true,
 
         // MooTools
         'resources/mootools/1.6.0/mootools-core.min.jsm': true,
