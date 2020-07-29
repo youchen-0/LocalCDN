b35ab41f5ed7bc9d7d91bd29aee9d57c4e4fa4bf
nobody <info@localcdn.org>
2020-07-29 07:34:07 +0200

    Improved: Handling with blocked resources

--- core/interceptor.js
+++ core/interceptor.js
@@ -35,11 +35,7 @@ interceptor.handleRequest = function (requestDetails, tabIdentifier, tab) {
     validCandidate = requestAnalyzer.isValidCandidate(requestDetails, tab);
 
     if (requestDetails.url.startsWith('https://fonts.googleapis.com/css?family')) {
-        if(interceptor.blockGoogleFonts  === true || interceptor.blockMissing === true) {
-            return {
-                'cancel': true
-            };
-        } else {
+        if(interceptor.blockGoogleFonts  !== false && interceptor.blockMissing !== false) {
             return {
                 'cancel': false
             };
