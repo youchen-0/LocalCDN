3df3b5f3a98f3d7718c2d181def431c9d2af234b
nobody <info@localcdn.org>
2020-11-18 06:18:37 +0100

    Updated: Socket.io v3.0.1 -> v3.0.2

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -375,7 +375,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/socket.io/2.')) {
         return '2.3.0';
     } else if (type.startsWith('/socket.io/3.')) {
-        return '3.0.1';
+        return '3.0.2';
     } else if (type.startsWith('/spin.js/2.')) {
         return '2.3.2';
     } else if (type.startsWith('/spin.js/3.')) {
