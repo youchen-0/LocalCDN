7a7765474890f527be6572df9c4f5663387ec8d3
nobody <info@localcdn.org>
2020-11-25 05:47:07 +0100

    Updated: vanilla-lazyload v17.2.0 -> v17.3.0 (#185)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -412,7 +412,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/urlive/1.')) {
         return '1.1.1';
     } else if (type.startsWith('/vanilla-lazyload')) {
-        return '17.2.0';
+        return '17.3.0';
     } else if (type.startsWith('/videojs-seek-buttons/')) {
         return '1.6.0';
     } else if (type.startsWith('/video.js/')) {
