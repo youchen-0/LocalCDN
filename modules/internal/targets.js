f11784962d0f9ede4489398735f93f59a92bc7ef
nobody <info@localcdn.org>
2020-12-09 06:29:52 +0100

    Updated: d3 v6.3.0 -> v6.3.1 (#201)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -166,7 +166,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/d3/5.')) {
         return '5.16.0';
     } else if (type.startsWith('/d3/6.')) {
-        return '6.3.0';
+        return '6.3.1';
     } else if (type.startsWith('/d3-legend/2.')) {
         return '2.25.6';
     } else if (type.startsWith('/dexie/3.') || type.startsWith('/dexie/latest') ) {
