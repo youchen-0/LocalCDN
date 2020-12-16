44b762882a6e4a3b0f09ca4c6d0668c814460472
nobody <info@localcdn.org>
2020-12-16 05:22:12 +0100

    Updated: instantsearch.js v4.9.1 -> v4.9.2 (#206)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -223,7 +223,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/instantsearch.js/3.')) {
         return '3.7.0';
     } else if (type.startsWith('/instantsearch.js/4.')) {
-        return '4.9.1';
+        return '4.9.2';
     } else if (type.startsWith('/iScroll/5.')) {
         return '5.2.0';
     } else if (type.startsWith('/jets/0.')) {
