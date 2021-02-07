849ab3ec95dd54fc47604f3534d17b80c2000351
nobody <info@localcdn.org>
2021-02-07 08:05:06 +0100

    Popup improved

--- core/constants.js
+++ core/constants.js
@@ -212,7 +212,11 @@ const Links = {
     'DONATE': chrome.extension.getURL('pages/donate/donate.html'),
     'CHANGELOG': chrome.extension.getURL('pages/updates/updates.html'),
     'FAQ': chrome.extension.getURL('pages/help/help.html'),
-    'STATISTICS': chrome.extension.getURL('pages/statistics/statistics.html')
+    'FAQ_HTML_FILTER': chrome.extension.getURL('pages/help/help.html#html-filter'),
+    'STATISTICS': chrome.extension.getURL('pages/statistics/statistics.html'),
+    'LOCALCDN_TEST': 'https://www.localcdn.org/test',
+    'LOCALCDN_TEST_WEBSITE': 'https://www.localcdn.org/test/check?domain=',
+    'WEBLATE': 'https://hosted.weblate.org/projects/localcdn/',
 };
 
 const CDNs = {
