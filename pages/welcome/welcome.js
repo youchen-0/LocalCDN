acd4881dc1f393791d39308da2a42dec5311e140
nobody <info@localcdn.org>
2021-03-30 06:31:59 +0200

    Improved: Welcome page

---
+++ pages/welcome/welcome.js
@@ -0,0 +1,35 @@
+/**
+ * Welcome Page
+ * Belongs to LocalCDN (since 2020-02-26)
+ *
+ * @author      nobody
+ * @since       2020-03-29
+ *
+ * @license     MPL 2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+'use strict';
+
+
+/**
+ * Welcome
+ */
+
+var welcome = {};
+
+
+/**
+ * Private Methods
+ */
+
+welcome._onDocumentLoaded = function () {
+    if (!BrowserType.FIREFOX) {
+        document.getElementById('chromium-banner').style.display = 'block';
+    }
+};
+
+document.addEventListener('DOMContentLoaded', welcome._onDocumentLoaded);
