ca5a070d72a94230ee57e1f5a92de60ccb225506
nobody <info@localcdn.org>
2021-04-01 07:05:40 +0200

    Updated: socket.io v4.0.0 -> v4.0.1 (#341)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -487,7 +487,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/socket.io/3.')) {
         return '3.1.2';
     } else if (type.startsWith('/socket.io/4.')) {
-        return '4.0.0';
+        return '4.0.1';
     } else if (type.startsWith('/spin.js/2.')) {
         return '2.3.2';
     } else if (type.startsWith('/spin.js/3.')) {
