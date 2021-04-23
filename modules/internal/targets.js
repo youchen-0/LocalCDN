1ae91caa2e17cb36312c4b25ecd5c671bb345e21
nobody <info@localcdn.org>
2021-04-23 06:59:13 +0200

    Updated: algoliasearch v4.8.6 -> v4.9.0 (#392)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -72,7 +72,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/algoliasearch/3.')) {
         return '3.35.1';
     } else if (type.startsWith('/algoliasearch/4.')) {
-        return '4.8.6';
+        return '4.9.0';
     } else if (type.startsWith('/anchor-js/3.')) {
         return '3.2.2';
     } else if (type.startsWith('/anchor-js/4.')) {
