40dcf73bf402b36d2eb391451dfe5d6ba52dcb07
nobody <info@localcdn.org>
2021-04-24 06:19:44 +0200

    Update: slick-carousel v1.8.1 -> v1.9.0 (#410)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -494,7 +494,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/simplemde/')) {
         return '1.11.2';
     } else if (type.startsWith('/slick-carousel/1.')) {
-        return '1.8.1';
+        return '1.9.0';
     } else if (type.startsWith('/slick-lightbox/0.')) {
         return '0.2.12';
     } else if (type.startsWith('/slider-pro/1.')) {
