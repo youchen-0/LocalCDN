a41792189ae1c40b6a0f1e88ce446d5b9cbc548c
nobody <info@localcdn.org>
2021-05-27 06:17:50 +0200

    Updated: dayjs v1.10.4 -> v1.10.5 (#474)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -237,7 +237,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/datatables/1.')) {
         return '1.10.24';
     } else if (type.startsWith('/dayjs/1.')) {
-        return '1.10.4';
+        return '1.10.5';
     } else if (type.startsWith('/dexie/3.') || type.startsWith('/dexie/latest')) {
         return '3.0.3';
     } else if (type.startsWith('/docsearch.js/2.')) {
