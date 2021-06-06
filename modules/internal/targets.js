c96965a9dd269545921c05d7e99aba9d8f5b324a
nobody <info@localcdn.org>
2021-06-06 06:12:43 +0200

    Updated: bodymovin v5.7.10 -> v5.7.11 (#482)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -178,7 +178,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/bodymovin/4.')) {
         return '4.13.0';
     } else if (type.startsWith('/bodymovin/5.')) {
-        return '5.7.10';
+        return '5.7.11';
     } else if (type.startsWith('/bootbox.js/4.')) {
         return '4.4.0';
     } else if (type.startsWith('/bootbox.js/5.')) {
