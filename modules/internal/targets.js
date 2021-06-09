4ad846038f66ab9a0ff443e0af02ae27a819090a
nobody <info@localcdn.org>
2021-06-09 06:32:12 +0200

    Updated: highlight.js v11.0.0 -> v11.0.1 (#493)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -291,7 +291,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/google-material-design-icons/')) {
         return 'v88';
     } else if (type.startsWith('/highlight.js/11.')) {
-        return '11.0.0';
+        return '11.0.1';
     } else if (type.startsWith('/highlight.js/10.')) {
         return '10.7.3';
     } else if (type.startsWith('/highlight.js/9.')) {
