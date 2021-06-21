2d8914cd0670ba9585653ce43231fb2e28888db8
nobody <info@localcdn.org>
2021-06-21 06:16:45 +0200

    Updated: vue-i18n v8.24.4 -> v8.24.5 (#531)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -608,7 +608,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/vue/3.')) {
         return '3.1.1';
     } else if (type.startsWith('/vue-i18n/8.')) {
-        return '8.24.4';
+        return '8.24.5';
     } else if (type.startsWith('/vue-i18n/9.')) {
         return '9.1.6';
     } else if (type.startsWith('/vue-resource/1.')) {
