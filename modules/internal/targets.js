37cb345a65b52e3e8d2755ef322b88e5a9ed1f02
nobody <info@localcdn.org>
2021-06-29 06:39:54 +0200

    Updated: mdb-ui-kit v3.8.0 -> v3.8.1 (#547)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -448,7 +448,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/mathjax/2.') || type.startsWith('/mathjax/latest')) {
         return '2.7.5';
     } else if (type.startsWith('/mdb-ui-kit/3.')) {
-        return '3.8.0';
+        return '3.8.1';
     } else if (type.startsWith('/mobile/1.')) {
         return '1.4.5';
     } else if (type.startsWith('/Modaal/0.')) {
