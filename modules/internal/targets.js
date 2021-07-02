78b6f7927fc6a6b6454a09e4aa0743201bd464a7
nobody <info@localcdn.org>
2021-07-02 06:38:40 +0200

    Updated: markdown-it v12.0.6 -> v12.1.0 (#551)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -429,7 +429,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/magnific-popup.js/1.')) {
         return '1.1.0';
     } else if (type.startsWith('/markdown-it/')) {
-        return '12.0.6';
+        return '12.1.0';
     } else if (type.startsWith('/mdbootstrap/4.')) {
         return '4.19.2';
     } else if (type.startsWith('/material-design-icons/2.')) {
