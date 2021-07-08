eb5fe4cc7f0989c5e9cad1f63414bacffd958c78
nobody <info@localcdn.org>
2021-07-08 06:44:11 +0200

    Updated: lightgallery v2.1.7 -> v2.1.8 (#548)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -426,7 +426,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/lightgallery/1.')) {
         return '1.10.0';
     } else if (type.startsWith('/lightgallery/2.')) {
-        return '2.1.7';
+        return '2.1.8';
     } else if (type.startsWith('/lodash.js/3.')) {
         return '3.10.1';
     } else if (type.startsWith('/lodash.js/4.')) {
