459041d7be88bd0288756d0e8cd7f725ea1396e6
nobody <info@localcdn.org>
2021-07-17 07:00:30 +0200

    Updated: vue v3.1.4 -> v3.1.5 (#532)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -621,7 +621,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/vue/2.')) {
         return '2.6.14';
     } else if (type.startsWith('/vue/3.')) {
-        return '3.1.4';
+        return '3.1.5';
     } else if (type.startsWith('/vue-i18n/8.')) {
         return '8.25.0';
     } else if (type.startsWith('/vue-i18n/9.')) {
