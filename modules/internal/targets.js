e19f32c654c696d02f307fcef7bacdf94756aab2
nobody <info@localcdn.org>
2021-08-02 06:42:23 +0200

    highlight.js v11.1.0 -> v11.2.0 (#594)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -314,7 +314,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/highlight.js/10.')) {
         return '10.7.3';
     } else if (type.startsWith('/highlight.js/11.')) {
-        return '11.1.0';
+        return '11.2.0';
     } else if (type.startsWith('/history/4.')) {
         return '4.10.1';
     } else if (type.startsWith('/history/5.')) {
