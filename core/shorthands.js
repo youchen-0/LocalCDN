1919cb555cf37ee224058ea8e9dc19e251cc3159
Calum McConnell <calumlikesapplepie@gmail.com>
2021-08-02 21:41:17 -0400

    Fix vue special-case mapping always matching

--- core/shorthands.js
+++ core/shorthands.js
@@ -201,7 +201,7 @@ shorthands.specialFiles = function (channelHost, channelPath, searchString) {
             'path': `resources/materialize/1.0.0/${channelPath}`,
             'bundle': 'Materialize'
         };
-    } else if (/cdn\.jsdelivr\.net\/npm\/vue@(2|3)/.test(CompleteURL)) {
+    } else if (/cdn\.jsdelivr\.net\/npm\/vue@(2|3)$/.test(CompleteURL)) {
         let version = CompleteURL.slice(-1);
         lastVersion = targets.setLastVersion(`/vue/${version}.`);
         if (lastVersion === false) {
