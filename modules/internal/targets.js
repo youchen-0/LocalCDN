be20d8ec34bd1d94541588f647d1e42136a1593e
nobody <info@localcdn.org>
2021-08-05 06:16:24 +0200

    font-awesome v5.15.3 -> v5.15.4 (#611)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -298,7 +298,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/font-awesome/4.')) {
         return '4.7.0';
     } else if (type.startsWith('/font-awesome/5.')) {
-        return '5.15.3';
+        return '5.15.4';
     } else if (type.startsWith('/gsap/1.')) {
         return '1.20.5';
     } else if (type.startsWith('/gsap/2.')) {
