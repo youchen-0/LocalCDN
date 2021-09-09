9c2c7a6a71b7e35baa5e29c736b3a61707561c2e
nobody <info@localcdn.org>
2021-09-09 05:44:13 +0200

    Swiper v7.0.3 -> v7.0.4 (#653)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -599,7 +599,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/Swiper/6.')) {
         return '6.8.4';
     } else if (type.startsWith('/Swiper/7.') || type.startsWith('/Swiper/')) {
-        return '7.0.3';
+        return '7.0.4';
     } else if (type.startsWith('/tether/1.')) {
         return '1.4.7';
     } else if (type.startsWith('/tooltipster/3.')) {
