4b167ea01b24a50f4fd78c2380470922ed06ea70
nobody <info@localcdn.org>
2021-10-01 05:56:12 +0200

    popper.js v2.10.1 -> v2.10.2 (#707)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -502,7 +502,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/popper.js/1.')) {
         return '1.16.1';
     } else if (type.startsWith('/popper.js/2.')) {
-        return '2.10.1';
+        return '2.10.2';
     } else if (type.startsWith('/prop-types/15.')) {
         return '15.7.2';
     } else if (type.startsWith('/protonet-jquery.inview/1.')) {
