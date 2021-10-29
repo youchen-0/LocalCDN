8aa0ba589e9d22e424f0002d0e7ab0d70f54d967
nobody <info@localcdn.org>
2021-10-29 06:17:02 +0200

    redux v4.1.1 -> v4.1.2 (#750)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -534,7 +534,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/react-lifecycles-compat/')) {
         return '3.0.4';
     } else if (type.startsWith('/redux/4.')) {
-        return '4.1.1';
+        return '4.1.2';
     } else if (type.startsWith('/rickshaw/1.')) {
         return '1.7.1';
     } else if (type.startsWith('/script.js/2.')) {
