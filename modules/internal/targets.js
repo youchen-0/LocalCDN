1df4cbd5da83715f57f89992de3d2b4b376f5ac9
nobody <info@localcdn.org>
2021-11-03 05:40:05 +0100

    vue v3.2.20 -> v3.2.21 (#756)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -637,7 +637,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/vue/2.')) {
         return '2.6.14';
     } else if (type.startsWith('/vue/3.')) {
-        return '3.2.20';
+        return '3.2.21';
     } else if (type.startsWith('/vue-i18n/8.')) {
         return '8.26.5';
     } else if (type.startsWith('/vue-i18n/9.')) {
