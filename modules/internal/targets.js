096aa92727f973a148028ade10cf6a563e2d1ab7
nobody <info@localcdn.org>
2021-11-19 06:40:56 +0100

    socket.io v4.3.2 -> v4.4.0 (#780)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -573,7 +573,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/socket.io/3.')) {
         return '3.1.3';
     } else if (type.startsWith('/socket.io/4.')) {
-        return '4.3.2';
+        return '4.4.0';
     } else if (type.startsWith('/spin.js/2.')) {
         return '2.3.2';
     } else if (type.startsWith('/spin.js/3.')) {
