4e7379f73d9dc6b7c428eb626cf2edb7a1a1d570
nobody <info@localcdn.org>
2021-12-04 07:54:26 +0100

    alpinejs v3.5.2 -> v3.7.0 (#789)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -84,7 +84,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/alpinejs/2.')) {
         return '2.8.2';
     } else if (type.startsWith('/alpinejs/3.')) {
-        return '3.5.2';
+        return '3.7.0';
     } else if (type.startsWith('/anchor-js/3.')) {
         return '3.2.2';
     } else if (type.startsWith('/anchor-js/4.')) {
