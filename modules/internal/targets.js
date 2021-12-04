794602b4887d3cb4f4801096c174448ea5f2a5b0
nobody <info@localcdn.org>
2021-12-04 08:08:11 +0100

    libphonenumber-js v1.9.43 -> v1.9.44 (#796)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -423,7 +423,7 @@ targets.setLastVersion = function (type, version) {
     } else if (type.startsWith('/leaflet.markercluster/1.')) {
         return '1.5.3';
     } else if (type.startsWith('/libphonenumber-js/1.')) {
-        return '1.9.43';
+        return '1.9.44';
     } else if (type.startsWith('/libsodium-wrappers/0.')) {
         return '0.5.4';
     } else if (type.startsWith('/lightcase/2.')) {
