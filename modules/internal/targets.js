11b13da13978d60b887a8298c619a7e3467b8c13
nobody <info@localcdn.org>
2022-01-05 06:22:48 +0100

    popper.js v2.11.0 -> v2.11.2 (#830)

--- modules/internal/targets.js
+++ modules/internal/targets.js
@@ -320,7 +320,7 @@ targets.setLastVersion = function (type, version) {
     if (type.startsWith('/paginationjs/2.')) return '2.1.5';
     if (type.startsWith('/plyr/3.')) return '3.6.12';
     if (type.startsWith('/popper.js/1.')) return '1.16.1';
-    if (type.startsWith('/popper.js/2.')) return '2.11.0';
+    if (type.startsWith('/popper.js/2.')) return '2.11.2';
     if (type.startsWith('/prop-types/15.')) return '15.8.0';
     if (type.startsWith('/protonet-jquery.inview/1.')) return '1.1.2';
     if (type.startsWith('/prototype/1.')) return '1.7.3';
